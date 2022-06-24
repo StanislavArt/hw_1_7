d540688f62be6c398d1c5fbe4fa44ac1323386be
Stanislav <artemiev.stanislav@gmail.com>
2022-06-24 22:33:27 +0700

    homework 1.7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        // task1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
 
+        System.out.printf("ФИО сотрудника — %s%n", fullName );
+
+        //task2
+        System.out.printf("Данные ФИО сотрудника для заполнения отчета — %s%n", fullName.toUpperCase());
+
+        //task3
+        fullName = "Иванов Семён Семёнович";
+        System.out.printf("Данные ФИО сотрудника — %s%n", fullName.replace('ё', 'е'));
     }
 }
